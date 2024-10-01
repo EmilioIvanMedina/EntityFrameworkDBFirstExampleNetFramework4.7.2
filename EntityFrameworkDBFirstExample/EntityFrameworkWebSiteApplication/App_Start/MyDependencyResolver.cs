7d56cf9f0096e7370dc909db2fa559085a0a1755
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-10-01 17:25:09 -0300

    add dependency injection container to the web site application project

---
+++ EntityFrameworkDBFirstExample/EntityFrameworkWebSiteApplication/App_Start/MyDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace EntityFrameworkWebSiteApplication.App_Start
+{
+    public class MyDependencyResolver : IDependencyResolver
+    {
+        private IServiceProvider _serviceProvider;
+
+        public MyDependencyResolver(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _serviceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _serviceProvider.GetServices(serviceType);
+        }
+    }
+}
